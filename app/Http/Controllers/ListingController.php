48c3ca674a80728275cbde49a5c726c956ef203b
salah <eljettiouisalaheddine@gmail.com>
2023-03-19 21:37:29 +0000

    working on profile page

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -26,15 +26,15 @@ class ListingController extends Controller
         $filters = $request->only([
             'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo'
         ]);
-        $query = Listing::orderByDesc('created_at')->when(
+        $query = Listing::mostRecent()->when(
             $filters['priceFrom'] ?? false,
             fn ($query, $value) => $query->where('price', '>=', $value)
         )->when(
             $filters['beds'] ?? false,
-            fn ($query, $value) => $query->where('beds', $value)
+            fn ($query, $value) => $query->where('beds', (int)$value < 6 ? '=' : '>=', $value)
         )->when(
             $filters['baths'] ?? false,
-            fn ($query, $value) => $query->where('baths', $value)
+            fn ($query, $value) => $query->where('baths', (int)$value < 6 ? '=' : '>=', $value)
         )->when(
             $filters['areaFrom'] ?? false,
             fn ($query, $value) => $query->where('area', '>=', $value)
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -21,5 +22,9 @@ class Listing extends Model
             \App\Models\User::class,
             'by_user_id'
         );
+    }
+    public function scopeMostRecent($query)
+    {
+        return $query->orderByDesc('created_at');
     }
 }
